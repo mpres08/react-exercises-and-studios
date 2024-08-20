9c386bb2668218d9717c486b2ec23e25d5d11487
mpres08 <158608642+mpres08@users.noreply.github.com>
2024-08-20 12:46:22 -0500

    completed components

--- part1/exercises/src/components/HobbyLinks.jsx
+++ part1/exercises/src/components/HobbyLinks.jsx
@@ -1,5 +1,18 @@
-function HobbyLinks() {
+import React from 'react';
+
+export default function HobbyLinks() {
     const hobbyLinks = [
-        
-    ]
+        "https://www.flyball.org/",
+        "https://www.amctheatres.com/"
+    ];
+
+    return(
+        <div>
+            <h3>My hobbies</h3>
+            <ul>
+                <li><a href = {hobbyLinks[0]}>Flyball</a></li>
+                <li><a href = {hobbyLinks[1]}>Going to the theater</a></li>
+            </ul>
+        </div>
+    )
 }
