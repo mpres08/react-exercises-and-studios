2b6d2641149f575385f82e26f8c0a9e576ad281a
mpres08 <158608642+mpres08@users.noreply.github.com>
2024-08-27 13:04:45 -0500

    upload studio

--- part3/studio/src/components/BoardAssignment.jsx
+++ part3/studio/src/components/BoardAssignment.jsx
@@ -1,15 +1,37 @@
 import { useState } from 'react';
 
 export default function BoardAssignment () {
-   const boards = [];
+
+   const [ boardName, setBoardName ] = useState("No boards yet!");
+
+   const boards = [
+      {
+         label: "New Recipes to Try",
+         value: "newRecipes"
+      },
+      {
+         label: "Desserts",
+         value: "desserts"
+      },
+      {
+         label: "Potluck Possibilities",
+         value: "potluckpossibilities"
+      }
+   ];
+
+   let boardOptions = boards.map((board, index) => {
+      return <option key={index} value={board.value}>{board.label}</option>;
+   });
 
    const handleChange = (event) => {
+      setBoardName(event.target.value)
    }
 
    return (
       <div style={{paddingTop: "50px"}}>
       <label>Save to Board: </label>
       <select value={boardName} onChange={handleChange}>
+         {boardOptions}
       </select>
 
       <p>Saved to {boardName}!</p>
--- part3/studio/src/components/Recipe.jsx
+++ part3/studio/src/components/Recipe.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 
 const RecipeAuthor = () => {
-   let authorLink = "";
-   let authorPhoto = "";
-   let authorName = "";
+   let authorLink = "https://littlespoonfarm.com/apple-pie-recipe/";
+   let authorPhoto = "https://littlespoonfarm.com/wp-content/uploads/2024/05/Amy.png";
+   let authorName = "Amy Duska";
 
    return (
       <div>
-         <img src={authorPhoto} alt = "" style={{objectFit: "contain", borderRadius: "50%"}} />
+         <img src={authorPhoto} alt = "" style={{objectFit: "contain", borderRadius: "50%"}} width="200px"/>
          <div>
             <h3>{authorName}</h3>
             <a href={authorLink}></a> 
@@ -17,16 +17,27 @@ const RecipeAuthor = () => {
 }
 
 const RecipeIngredients = () => {
-   const ingredients = [];
+   const ingredients = [
+      "2 9-inch pie crusts",
+      "7 large Granny Smith apples (peeled, cored and sliced into 1/2 inch slices)",
+      "1/2 cup granulated sugar",
+      "1/2 cup light brown sugar (loosely packed)",
+      "2 tablespoons all-purpose flour",
+      "1 teaspoon ground cinnamon",
+      "1/8 teaspoon ground nutmeg",
+      "1 tablespoon lemon juice (plus the zest of half a lemon)",
+      "1 large egg (lightly beaten in a small bowl for egg wash)",
+      "2 tablespoons sanding sugar (optional)"
+   ];
+
+   let ingredientListItems = ingredients.map((ingredient, index) => {
+      return <li key={index}>{ingredient}</li>;
+   })
    return(
       <div>
          <h3>Recipe Ingredients</h3>
          <ul>
-            <li>{ingredients[0]}</li>
-            <li>{ingredients[1]}</li>
-            <li>{ingredients[2]}</li>
-            <li>{ingredients[3]}</li>
-            <li>{ingredients[4]}</li>
+            {ingredientListItems}
          </ul>
       </div>
    );
@@ -36,8 +47,8 @@ const RecipeDescription = () => {
    return (
       <div> 
          <div>
-            <h1></h1>
-            <p></p>
+            <h1>Easy Apple Pie</h1>
+            <p>This is hands down the BEST and easiest apple pie recipe! It has a tender, flaky, homemade pie crust with apple slices drenched in sugar and warm spices like cinnamon and nutmeg.</p>
          </div>
          <div className="recipePhotoBlock">
             <RecipeIngredients />
@@ -49,7 +60,7 @@ const RecipeDescription = () => {
 
 const RecipePhoto = () => {
    return (
-      <img src="" alt="" className="imageUpdates"/>
+      <img src="https://littlespoonfarm.com/wp-content/uploads/2021/08/Homemade-Apple-Pie-Recipe-2-2.jpg" alt="A photo of apple pie" className="imageUpdates" height="450px"/>
    );
 }
 
